d548a1a0cfce875b98c3ad3755cb6f06bd722d6f
Okiban-Nosobi <142680254+Okiban-Nosobi@users.noreply.github.com>
2023-11-04 14:15:33 +0100

    Update launch.sh
    fix polybar launch.sh to grep the word connected

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,7 +3,7 @@
 killall polybar
 
 if type "xrandr";then
-	for m in $(xrandr --query | grep "connected" | cut -d" " -f1); do
+	for m in $(xrandr --query | grep -w "connected" | cut -d" " -f1); do
 		MONITOR=$m polybar --reload okiban &
 	done
 else
